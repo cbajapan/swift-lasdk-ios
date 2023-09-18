717f96e6cd3a497ca0f0eee4998d69018c2eae24
Cole Moore <Cartisim@icloud.com>
2023-09-18 11:43:30 +0400

    Added Beta Podfiles

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
                 "WebRTC"
             ]
         ),
-        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0.xcframework.zip", checksum: "3839c358dbe0fdfd536affec2daf8cf8172cc5bcd2f1fabfebe61e4cf0d2a05e"),
+        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0-beta.2.xcframework.zip", checksum: "e6764fab1c661c824d6f3b7b9809995294f4b20401f85f15d51062ec97699ace"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
