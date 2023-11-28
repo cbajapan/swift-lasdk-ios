ad48d4604b56074136f89a07ccf9c89714c4b3a3
Cole Moore <Cartisim@icloud.com>
2023-11-28 16:42:15 +0400

    Updated for beta 4

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
                 "WebRTC"
             ]
         ),
-        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0-beta.2.xcframework.zip", checksum: "e6764fab1c661c824d6f3b7b9809995294f4b20401f85f15d51062ec97699ace"),
+        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0-beta.4.xcframework.zip", checksum: "a6d07b45371e60b7b709c7cb2888321231dea2b540335b21a88ab18b794a6642"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m110.xcframework.zip", checksum: "2750cf1087b2441d67208ca2b0905578b4ad1797a68d2d2758d0f075500f0011")
     ]
 )
