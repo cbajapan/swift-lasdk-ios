51a3ff5118d9957b07659acb7153a8139b73056c
cba-githubdeploy <cba-adoservconn@cba-japan.com>
2024-06-12 00:55:47 +0000

    Azure Pipelines: Add new version of package checksum

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
                 "WebRTC"
             ]
         ),
-        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0-beta7.xcframework.zip", checksum: "9923b31d65c354488ef39c4b71f88dd410ce0caf26120cc5e4e81e909fd88920"),
+        .binaryTarget(name: "LASDKiOS", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/lasdk/LASDKiOS-2.0.0-beta8.xcframework.zip", checksum: "dcd1b968d63a2527807efa7b6896432f1831d006e3398d242ef3a344f749c4cd"),
         .binaryTarget(name: "WebRTC", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/WebRTC-m117.xcframework.zip", checksum: "7950151686d41d8fc33e69a9ad5bd47cad24d20691b4167e9fa4d8d156a8fca6")
     ]
 )
